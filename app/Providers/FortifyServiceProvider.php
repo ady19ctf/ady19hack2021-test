cfea85dc4cd6240e18aaaa87a4c315a52d937633
ady19ctf <ady19ctf@gmail.com>
2021-12-30 18:37:21 +0900

    ログイン: email -> username へ変更

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -11,6 +11,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
+use Laravel\Fortify\Http\Requests\LoginRequest;
+use App\Models\User;
+use Hash;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -34,8 +37,18 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
-        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
-
+	Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+	Fortify::authenticateUsing(function (LoginRequest $request) {
+	    $user = User::where('email', $request->identity)
+	        ->orWhere('username', $request->identity)->first();
+	    if (
+                $user &&
+                Hash::check($request->password, $user->password)
+            ) {
+                return $user;
+	    }
+	});
+/*
         RateLimiter::for('login', function (Request $request) {
             $email = (string) $request->email;
 
@@ -44,6 +57,7 @@ class FortifyServiceProvider extends ServiceProvider
 
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
-        });
+	});
+*/
     }
 }
