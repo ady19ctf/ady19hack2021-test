47de8fd357923c78f7f304f57b9653ee834d3f3d
ady19ctf <ady19ctf@gmail.com>
2022-01-01 03:21:37 +0900

    UpdateUserProfileInformationのカラム追加完了

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -35,6 +35,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             $user->forceFill([
                 'name' => $input['name'],
                 'email' => $input['email'],
+                'birth' => $input['birth'],
             ])->save();
         }
     }
--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -59,6 +59,13 @@
             <x-jet-input-error for="name" class="mt-2" />
         </div>
 
+        <!-- Birthday -->
+        <div class="col-span-6 sm:col-span-4">
+            <x-jet-label for="birth" value="{{ __('Birthday') }}" />
+            <x-jet-input id="birth" type="text" class="mt-1 block w-full" wire:model.defer="state.birth" autocomplete="birth" />
+            <x-jet-input-error for="birth" class="mt-2" />
+        </div>
+
         <!-- Email -->
         <div class="col-span-6 sm:col-span-4">
             <x-jet-label for="email" value="{{ __('Email') }}" />
