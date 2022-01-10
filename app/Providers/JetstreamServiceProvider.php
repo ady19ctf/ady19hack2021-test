10ab1917856c5d261df8125dd4855df43842041b
ady19ctf <ady19ctf@gmail.com>
2022-01-10 18:40:36 +0900

    新規ブランチを作り、さらにその子ブランチを新規に作ってmainにマージすると、子ブランチの変更履歴しかマージされないみたいので注意

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -2,9 +2,13 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use App\Actions\Jetstream\DeleteUser;
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Jetstream\Jetstream;
+use Laravel\Fortify\Fortify;
 
 class JetstreamServiceProvider extends ServiceProvider
 {
@@ -28,8 +32,17 @@ class JetstreamServiceProvider extends ServiceProvider
         $this->configurePermissions();
 
         Jetstream::deleteUsersUsing(DeleteUser::class);
-    }
 
+        Fortify::authenticateUsing(function (Request $request) {
+	    $user = User::where('name', $request->name)
+	    ->first();
+
+            if ($user &&
+                Hash::check($request->password, $user->password)) {
+                return $user;
+            }
+	});
+    }
     /**
      * Configure the permissions that are available within the application.
      *
