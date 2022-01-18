ee3eb2bd156a4cd98eb991f53d1394a8c0ce1503
ady19ctf <ady19ctf@gmail.com>
2022-01-18 23:23:58 +0900

    take in the diff of main branch

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\User;
 use App\Actions\Jetstream\DeleteUser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Jetstream\Jetstream;
@@ -20,6 +21,9 @@ class JetstreamServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if (App::environment('production')) {
+          $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
