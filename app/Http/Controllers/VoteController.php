2aafdfc267e67f41f54cde082d118688cc680537
ady19ctf <ady19ctf@gmail.com>
2022-01-23 03:14:35 +0900

    work awwith s secret manager t

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -10,21 +10,23 @@ use Aws\SecretsManager\SecretsManagerClient;
 use Aws\Exception\AwsException;
 
 
+
 class VoteController extends Controller
 {
     public function view(){
+        logger('in view func');
         $hosts = array('test-chain00', 'test-chain01');
-
         $user_address = Auth::user()['address'];
         $path=resource_path();
 
-        
-        
-        $admin_host=$this->getAPIFactor('test-chain00-ip');
-        $port=$this->getAPIFactor('test-chain00-port');
-        $ruser=$this->getAPIFactor('test-chain00-user');
-        $rpass=$this->getAPIFactor('test-chain00-pass');
-        logger('aws api debug', [$ruser,$rpass]);
+        $own_wallet=$this->getOwnWalletHost($hosts, $user_address);
+        logger('own wallet host',[$own_wallet]);
+
+        $admin_host=$this->getAPIFactor("{$own_wallet}-ip");
+        $port=$this->getAPIFactor("{$own_wallet}-port");
+        $ruser=$this->getAPIFactor("{$own_wallet}-user");
+        $rpass=$this->getAPIFactor("{$own_wallet}-pass");
+        logger('JSON RPC INFO', [$ruser,$rpass]);
 
         $python_code="python3 $path/python/show_asset.py $admin_host $port $ruser $rpass $user_address";
         exec($python_code, $output, $status);
@@ -33,19 +35,23 @@ class VoteController extends Controller
     }
 
     public function check(){
-        // logger('test',[$_POST]);
         return view('/vote-check',['candidate'=>$_POST['candidate']]);
     }
 
     public function vote(){
+        logger('in vote func');
+        $hosts = array('test-chain00', 'test-chain01');
         $user_address = Auth::user()['address'];
-        $user_name = Auth::user()['name'];
         $path=resource_path();
+        logger('own wallet host',[$hosts]);
+        $own_wallet=$this->getOwnWalletHost($hosts, $user_address);
+        logger('own wallet host',[$own_wallet]);
 
-        $admin_host=$this->getAPIFactor('test-chain00-ip');
-        $port=$this->getAPIFactor('test-chain00-port');
-        $ruser=$this->getAPIFactor('test-chain00-user');
-        $rpass=$this->getAPIFactor('test-chain00-pass');
+        $admin_host=$this->getAPIFactor("{$own_wallet}-ip");
+        $port=$this->getAPIFactor("{$own_wallet}-port");
+        $ruser=$this->getAPIFactor("{$own_wallet}-user");
+        $rpass=$this->getAPIFactor("{$own_wallet}-pass");
+        logger('JSON RPC INFO', [$ruser,$rpass]);
 
         $candi_address='1URXJTtrc9gzWV9bWcmJnrAV6A8hQP88o4f4Xb';
         $asset_name='asset2';
@@ -58,11 +64,12 @@ class VoteController extends Controller
 
     
     public function getAPIFactor($key){
+        logger('in getAPIFactor func');
         $client = new SecretsManagerClient([
             'version' => '2017-10-17',
             'region' => 'ap-northeast-1',
         ]);
-        $secretName = env('SECRET_NAME');
+        $secretName = env('SECRET_NAME'); //written in .env file
         $result = $client->getSecretValue([
             'SecretId' => $secretName,
         ]);
@@ -109,5 +116,47 @@ class VoteController extends Controller
         $secret = json_decode($secret,true)[$key];
         
         return $secret;
+    }
+
+    public function getOwnWalletHost($hosts, $user_address){
+        logger('in getOwnWalletHost func');
+        $path=resource_path();
+        $get_address_flg=FALSE;
+        $own_wallet='';
+
+        foreach($hosts as $host){
+            $addresses='';
+            $admin_host=$this->getAPIFactor("{$host}-ip");
+            $port=$this->getAPIFactor("{$host}-port");
+            $ruser=$this->getAPIFactor("{$host}-user");
+            $rpass=$this->getAPIFactor("{$host}-pass");
+            logger('connecting chain server', [$admin_host,$port]);
+    
+            $python_code="python3 $path/python/get_addresses.py $admin_host $port $ruser $rpass";
+            exec($python_code, $addresses, $status);
+            logger('address result', [$status, $addresses]);
+            
+            //文字列型の配列をPHPの配列型に変換。いずれ正規表現で。
+            $addresses=str_replace(' \'', '', $addresses);
+            $addresses=str_replace('\'', '', $addresses);
+            $addresses=str_replace('[', '', $addresses);
+            $addresses=str_replace(']', '', $addresses);
+            $addresses=explode(",", $addresses[0]);
+    
+            $is_this_host=in_array($user_address, $addresses);
+            if($is_this_host){
+                logger('address is in', [$host]);
+                $get_address_flg=TRUE;
+                $own_wallet=$host;
+                break;
+            }else{
+                logger('address is not in', [$host]);            
+            }
+        }
+        if($get_address_flg==FALSE){
+            logger("address {$user_address} cannot be found...");
+        }
+
+        return $own_wallet;
     }
 }
