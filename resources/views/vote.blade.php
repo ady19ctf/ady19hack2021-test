92db247e53d9251c62241dad889f8730ac697311
ady19ctf <ady19ctf@gmail.com>
2022-01-22 12:40:20 +0900

    change the value of button to the candidate's name

--- resources/views/vote.blade.php
+++ resources/views/vote.blade.php
@@ -6,18 +6,20 @@
   <body>
     <h1>Vote for one candidate.</h1>
     <div>
-        <form action="/vote-result" method="post">
+
+        <form action="/vote-check" method="post">
             @csrf
-            <button class="btn btn-primary" name="vote_for_a" type="submit">candidate A</button>
+            <button class="btn btn-primary" name="candidate" value="candidate A" type="submit">candidate A</button>
         </form>
-        <form action="/vote-result" method="post">
+        <form action="/vote-check" method="post">
             @csrf
-            <button class="btn btn-primary" name="vote_for_b" type="submit">candidate B</button>
+            <button class="btn btn-primary" name="candidate" value="candidate B" type="submit">candidate B</button>
         </form>
-        <form action="/vote-result" method="post">
+        <form action="/vote-check" method="post">
             @csrf
-            <button class="btn btn-primary" name="vote_for_c" type="submit">candidate C</button>
+            <button class="btn btn-primary" name="candidate" value="candidate C" type="submit">candidate C</button>
         </form>
     </div>
   </body>
+  <a href="/home">Return to home.</a></br>
 </html>
