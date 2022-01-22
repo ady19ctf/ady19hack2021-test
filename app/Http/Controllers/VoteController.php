8abb827235e42245279c21557ccab415be50b00d
ady19ctf <ady19ctf@gmail.com>
2022-01-22 12:46:29 +0900

    add the function to get voting statement

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -7,6 +7,30 @@ use Illuminate\Support\Facades\Auth;
 
 class VoteController extends Controller
 {
+    
+    public function view(){
+        $user_address = Auth::user()['address'];
+        $path=resource_path();
+        
+        // 以下のサーバ情報等は別に保管する予定。一時的にベタ書き。Cred情報は別の場所に保管。
+        $admin_host='20.0.0.160';
+        $port='7416';
+        $ruser='multichainrpc';
+        $rpass='';
+
+        $python_code="python3 $path/python/show_asset.py $admin_host $port $ruser $rpass $user_address";
+        exec($python_code, $output, $status);
+        // logger('test',[$output[0],$status]);
+
+
+        return view('/statement',['result'=>$output[0]]);
+    }
+
+    public function check(){
+        // logger('test',[$_POST]);
+        return view('/vote-check',['candidate'=>$_POST['candidate']]);
+    }
+
     public function vote(){
         $user_address = Auth::user()['address'];
         $user_name = Auth::user()['name'];
@@ -15,28 +39,16 @@ class VoteController extends Controller
         // 以下のサーバ情報等は別に保管する予定。一時的にベタ書き。Cred情報は別の場所に保管。
         $admin_host='20.0.0.160';
         $port='7416';
-        $ruser='';
+        $ruser='multichainrpc';
         $rpass='';
 
-        if (isset($_POST['vote_for_a'])) {
-            $candidate_address='hogehoge';
-            return view('/vote-result',['result'=>$user_name]);
-        }
-        elseif (isset($_POST['vote_for_b'])) {
-            $candidate_address='hogehoge';
-            // logger('test',['foo' => 'bar']);
-            return view('/vote-result',['result'=>$user_address]);
-        }
-        elseif (isset($_POST['vote_for_c'])) {
-            $candidate_address='hogehoge';
-            logger('voting test', [$admin_host, $port, $ruser, $rpass, $user_address]);
-            $python_code="python3 $path/python/show_asset.py $admin_host $port $ruser $rpass $user_address";
-            exec($python_code, $output, $status);
-            // logger('test',[$python_code,$status]);
-            return view('/vote-result',['result'=>$output[0]]);
-        }
+        $candi_address='1URXJTtrc9gzWV9bWcmJnrAV6A8hQP88o4f4Xb';
+        $asset_name='asset2';
+
+        $python_code="python3 $path/python/vote_asset.py $admin_host $port $ruser $rpass $user_address $candi_address $asset_name";
+        exec($python_code, $output, $status);
         
-        return view('/vote-result',['result'=>$user_address]);
+        return view('/vote-result',['result'=>$output[0]]);
     }
 
     public function sendAsset(){
