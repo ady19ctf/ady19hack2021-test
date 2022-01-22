b612d91a20856954a2027ffd802ae969ddf24d6d
ady19ctf <ady19ctf@gmail.com>
2022-01-22 12:34:30 +0900

    add a user's home page for test

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>user home</title>
+  </head>
+  <body>
+    <h1>User Home</h1>
+    <div>
+       <a href="/view">投票実績を確認する</a></br>
+       <a href="/vote">投票する</a></br>
+    </div>
+  </body>
+</html>
