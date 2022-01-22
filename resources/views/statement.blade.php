464a01b120482e4461591dd86207070196d962bf
ady19ctf <ady19ctf@gmail.com>
2022-01-22 12:43:16 +0900

    add a view for show the voting statement

---
+++ resources/views/statement.blade.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>vote statement</title>
+  </head>
+  <body>
+    <h1>Remaining Votes</h1>
+    <div>残り投票数：{{$result}}</div>
+  </body>
+  <a href="/home">Return to home.</a></br>
+</html>
