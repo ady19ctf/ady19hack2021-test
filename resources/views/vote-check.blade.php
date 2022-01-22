5dcd06ef0b9e69919a5e1ac2322a52ca51eb0d67
ady19ctf <ady19ctf@gmail.com>
2022-01-22 12:36:19 +0900

    add a view for confirmation of voting

---
+++ resources/views/vote-check.blade.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>vote check</title>
+  </head>
+  <body>
+    <h1>Vote Check</h1>
+    <div>{{$candidate}}に投票します。よろしいですか？</div>
+  </body>
+  <form action="/vote-result" method="post">
+        @csrf
+        <button class="btn btn-primary" type="submit">Yes</button>
+  </form>
+  <form action="/vote" method="get">
+        @csrf
+        <button class="btn btn-primary" type="submit">No</button>
+  </form>
+
+  <a href="/home">Return to home.</a></br>
+</html>
